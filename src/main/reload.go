6efcaaf99340949f5f45538765e267081ccee86a
oras903 <352965446@qq.com>
2018-01-08 21:23:11 +0800

    Update reload.go

--- src/main/reload.go
+++ src/main/reload.go
@@ -91,7 +91,7 @@ json2 := `{"from":"en","to":"zh","ipAddresses":[{"ipAddress":"192.168.20.10","ds
                   
                }
 
-       fmt.Println("stopSSSSSSSSSS")
+       fmt.Println("stopuuuuuuuuuuuuuuuu")
        fmt.Println("++++")
        fmt.Println("++++")
        fmt.Println("++++")
